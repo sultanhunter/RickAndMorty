1dc7dc0197104f3469dc50e7ee6a1ab57b7e59f8
sultan <sultanibneusman0@gmail.com>
2023-12-14 01:17:24 +0530

    Api Services added

---
+++ RickAndMorty/APIClient/ApiEndpoint.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+@frozen enum ApiEndpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorty/APIClient/ApiService.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+/// Primary API Service object to get Rick and Morty Data
+final class ApiService {
+    static let shared = ApiService()
+
+    private init() {}
+
+    public func execute(_ request: ApiRequest, completion: @escaping () -> void) {}
+}
